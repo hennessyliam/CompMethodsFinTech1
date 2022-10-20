3e1c136a23effdba437eb1aacc116e8a47704400
hennessyliam <liam.hennessy@outlook.com>
2022-10-20 19:18:40 -0400

    test commit 2

---
+++ week6/dynamic_array.py
@@ -0,0 +1,10 @@
+my_list = [2, 5, 7, 11, 13, 17, 19]
+my_list.insert(1,3)
+
+# import ctypes
+class DynamicArray:
+	def __init__(self):
+		"""Create an empty array"""
+		self.n = 0 # count actual elements
+		self.capacity = 1 # default array capacity
+		self.A = self.
