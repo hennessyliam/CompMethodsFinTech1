ce9f0b3aa4199f71e56bb665dc2cc08b8be840fc
hennessyliam <liam.hennessy@outlook.com>
2022-10-28 17:05:55 -0400

    added way to see string with most zeroes in output

--- 6778 Docs/hashing_algo.py
+++ 6778 Docs/hashing_algo.py
@@ -1,6 +1,7 @@
 import os
 import secrets
 import hashlib
+import time
 
 a_string = ''
 hashed_string = ''
@@ -13,7 +14,7 @@ most_zero_string = ''
 # while the first 10 characters of the hashed string are not 0000000000
 while hashed_string[:11] != '0000000000':
 		# generate a random string
-		a_string = 'Anna Liam Nicholas Zachary ' + str(secrets.token_bytes())
+		a_string = 'Anna Liam Nicholas Zachary ' + str(secrets.token_bytes(10))
 		# hash the string
 		hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
 
@@ -23,6 +24,7 @@ while hashed_string[:11] != '0000000000':
 		print("Iterations:", iterations)
 		print('Max leading zeros:', max_leading_zeros)
 		print(hashed_string)
+		# updates 
 		print(most_zero_string)
 		
 		if hashed_string.startswith('0'):
