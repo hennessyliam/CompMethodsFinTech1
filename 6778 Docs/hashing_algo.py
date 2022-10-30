2fe1d87568f10b815ab07106e7f6403327e66020
hennessyliam <liam.hennessy@outlook.com>
2022-10-30 19:44:20 -0400

    restructure algorithm

--- 6778 Docs/hashing_algo.py
+++ 6778 Docs/hashing_algo.py
@@ -11,25 +11,28 @@ iterations = 0
 
 most_zero_string = ''
 
+begin = time.time()
+
 # while the first 10 characters of the hashed string are not 0000000000
-while hashed_string[:11] != '0000000000':
+while hashed_string[:3] != '000':
 		# generate a random string
-		a_string = 'Anna Liam Nicholas Zachary ' + str(secrets.token_bytes(10))
+		a_string = 'Anna Liam Nicholas Zachary ' + str(secrets.token_bytes(20))
 		# hash the string
 		hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
 
 		iterations += 1
+		
+		if hashed_string.startswith('0'):
+				leading_zeros = len(hashed_string) - len(hashed_string.lstrip('0'))
+				if leading_zeros > max_leading_zeros:
+					max_leading_zeros = leading_zeros 
+					most_zero_string = a_string
 
 		os.system('clear')
 		print("Iterations:", iterations)
 		print('Max leading zeros:', max_leading_zeros)
 		print(hashed_string)
-		# updates 
 		print(most_zero_string)
-		
-		if hashed_string.startswith('0'):
-				leading_zeros = len(hashed_string) - len(hashed_string.lstrip('0'))
-				if leading_zeros > max_leading_zeros:
-					most_zero_string = a_string
-					max_leading_zeros = leading_zeros
-                        
+
+end = time.time()
+print("Time taken in hours: " + str((end-begin)/360))
