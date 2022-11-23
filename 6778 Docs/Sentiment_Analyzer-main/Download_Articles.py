873c13e8b63be876b5d379e664889852a25ba0e0
hennessyliam <liam.hennessy@outlook.com>
2022-11-22 19:14:19 -0500

    init

--- 6778 Docs/Sentiment_Analyzer-main/Download_Articles.py
+++ 6778 Docs/Sentiment_Analyzer-main/Download_Articles.py
@@ -7,102 +7,117 @@ import time
 options = webdriver.ChromeOptions()
 
 # Github credentials
-username = "li621332"
-password = "Joseph1102!"
+username = "username"
+password = "password"
 
-company_name = "3M"
+# 'American Express', 'Honeywell', 'Coca-Cola', 'Goldman Sachs', 'Cisco', 'Chevron', 'Caterpillar', 'Boeing', 'Apple', 'Amgen', '3M',  'Dow', 'Home Depot', 'IBM', 'Intel', , 'JPMorgan Chase', "McDonald's", 'Merck', 'Microsoft', 'Nike', 'Procter & Gamble', 'Salesforce', 'Travelers', 'UnitedHealth', 'Verizon', 'Visa', 'Walgreens Boots Alliance', 'Walmart', 'Walt Disney'
 
-# Initialize the Chrome driver
-driver = webdriver.Chrome(options=Options())
-# Head to Nexis login page
-driver.get("https://guides.ucf.edu/database/LNA")
-
-# Maximize window and wait
-driver.maximize_window()
+companies = ['Amgen']
 
 
-# Login
-driver.find_element("id", "userNameInput").send_keys(username)
-driver.find_element("id", "passwordInput").send_keys(password)
-driver.find_element("id","submitButton").click()
+def download_articles(company_name):
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome(options=Options())
+    # Head to Nexis login page
+    driver.get("https://guides.ucf.edu/database/LNA")
 
-time.sleep(3)
-
-# New URL
-#print(driver.current_url)
-driver.get(driver.current_url)
-
-# Select type in company name
-
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/lng-search-input/div/div/lng-expanding-textarea").send_keys(str(company_name))
+    # Maximize window and wait
+    driver.maximize_window()
 
 
+    # Login
+    driver.find_element("id", "userNameInput").send_keys(username)
+    driver.find_element("id", "passwordInput").send_keys(password)
+    driver.find_element("id","submitButton").click()
 
-# Select date rang
-# date range button
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div/button").click()
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/ul/li[9]/button/span[1]").click()
+    time.sleep(3)
 
-# first date
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[3]/calendarfield/div/input").send_keys(Keys.COMMAND, "a")
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[3]/calendarfield/div/input").send_keys("07/15/2022")
+    # New URL
+    #print(driver.current_url)
+    driver.get(driver.current_url)
 
-# second date
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[4]/calendarfield/div/input").send_keys(Keys.COMMAND, "a")
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[4]/calendarfield/div/input").send_keys("08/15/2022")
+    # Select type in company name
 
-# apply date selection
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/input").click()
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/lng-search-input/div/div/lng-expanding-textarea").send_keys(str(company_name))
 
+    time.sleep(0.5)
 
-# change to 'news'
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div/button").click()
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div[2]/div/div/filter-selector-node[1]/label/span").click()
+    # Select date range
+    # date range button
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div/button").click()
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/ul/li[9]/button/span[1]").click()
 
-# click search button
-driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/lng-search-button").click()
+    # first date
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[3]/calendarfield/div/input").send_keys(Keys.COMMAND, "a")
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[3]/calendarfield/div/input").send_keys("07/15/2022")
 
-# Select Articles 1-10
+    # second date
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[4]/calendarfield/div/input").send_keys(Keys.COMMAND, "a")
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/div[4]/calendarfield/div/input").send_keys("08/15/2022")
 
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[2]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[4]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[6]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[8]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[10]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[12]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[14]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[16]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[18]/label/input").click()
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[20]/label/input").click()
-
-time.sleep(2)
-
-# click download button
-driver.find_element(By.XPATH, "//*[@id='results-list-toolbar-gvs']/ul[1]/li[4]/ul/li[3]/button/span[1]").click()
-
-time.sleep(1)
-# choose format (currently set to .docx)
-
-driver.find_element(By.ID, "Docx").click()
-
-time.sleep(1)
-
-# name file name of company
-
-driver.find_element(By.ID, "FileName").send_keys(Keys.COMMAND, "a")
-
-driver.find_element(By.ID, "FileName").clear()
-
-driver.find_element(By.ID, "FileName").send_keys(str(company_name))
+    # apply date selection
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/input").click()
 
 
+    # change to 'news'
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div/button").click()
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div[2]/div/div/filter-selector-node[1]/label/span").click()
 
-# Download
+    # click search button
+    driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/lng-search-button").click()
+    time.sleep(0.5)
+    # Select Articles 1-10
 
-driver.find_element(By.XPATH, "/html/body/aside/footer/div/button[1]").click()
-time.sleep(10)
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[2]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[4]/label/input").click()
+    time.sleep(0.25)
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[6]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[8]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[10]/label/input").click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[12]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[14]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[16]/label/input").click()
+    time.sleep(0.25)
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[18]/label/input").click()
+    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[20]/label/input").click()
+
+    time.sleep(2)
+
+    # click download button
+    driver.find_element(By.XPATH, "//*[@id='results-list-toolbar-gvs']/ul[1]/li[4]/ul/li[3]/button/span[1]").click()
+
+    time.sleep(2)
+
+    # choose format 
+    # .docx
+    #driver.find_element(By.ID, "Docx").click()
+    # PDF
+    #driver.find_element(By.ID, "PDF").click()
+    # .rtf
+    driver.find_element(By.ID, "Rtf").click()
+
+    time.sleep(2)
+
+    # name file name of company
+
+    driver.find_element(By.ID, "FileName").send_keys(Keys.COMMAND, "a")
+
+    driver.find_element(By.ID, "FileName").clear()
+
+    driver.find_element(By.ID, "FileName").send_keys(str(company_name))
 
 
-# close the driver
+
+    # Download
+
+    driver.find_element(By.XPATH, "/html/body/aside/footer/div/button[1]").click()
+    time.sleep(15)
 
 
+    # close the driver
+
+
+for company in companies:
+    company_name = company
+    download_articles(company_name)
--- 6778 Docs/Sentiment_Analyzer-main/Sentiment_Analyzer.py
+++ 6778 Docs/Sentiment_Analyzer-main/Sentiment_Analyzer.py
@@ -1,1 +1,95 @@
+# import libraries
+import csv
+from striprtf.striprtf import rtf_to_text
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+
+
+companies = ['3M', 'American Express', 'Amgen', 'Apple', 'Boeing', 'Caterpillar', 'Chevron', 'Cisco', 'Coca-Cola', 'Dow', 'Goldman Sachs', 'Home Depot', 'Honeywell', 'IBM', 'Intel', 'JPMorgan Chase', 'JohnsonJohnson', "McDonalds", 'Merck', 'Microsoft', 'Nike', 'Procter & Gamble', 'Salesforce', 'Travelers', 'UnitedHealth', 'Verizon', 'Visa', 'Walgreens Boots Alliance', 'Walmart', 'Walt Disney']
+
+# count num of positive words in each article
+positive_count = 0
+negative_count = 0
+
+# find positive word file list
+positive_word_file = csv.reader(open('/Users/hennessyliam/CompMethodsFinTech1/6778 Docs/Loughran_Word_Lists/LoughranMcDonald_Positive.csv', 'r'))
+
+# find negative word file list
+negative_word_file = csv.reader(open('/Users/hennessyliam/CompMethodsFinTech1/6778 Docs/Loughran_Word_Lists/LoughranMcDonald_Negative.csv', 'r'))
+
+# add positive words to python list
+positive_words = []
+for row in positive_word_file:
+    for word in row:
+        positive_words.append(word)
+
+# add negative words to python list
+negative_words = []
+for row in negative_word_file:
+    for word in row:
+        negative_words.append(word)
+
+
+listx = []
+listpos = []
+listneg = []
+listscore = []
+
+def get_sentiment_score(positive_count: int, negative_count: int):
+    count = 0
+   
+# open company article
+    with open('/Users/hennessyliam/CompMethodsFinTech1/6778 Docs/Sentiment_Analyzer-main/DJIA_Compiled_Articles/' + company_name + '.RTF', 'r') as f:
+        content = f.read()
+        text = rtf_to_text(content, encoding='latin-1', errors='ignore').split()
+
+
+        for word in text:
+            count +=1
+            if word.upper() in positive_words:
+                positive_count += 1
+            if word.upper() in negative_words:
+                negative_count -= 1
+        
+        sentiment_score = (positive_count + negative_count)
+        score_per_total_words = (positive_count - negative_count) / count
+
+        listx.append(company_name)
+        listpos.append(positive_count)
+        listneg.append(negative_count)
+        listscore.append(sentiment_score)
+
+
+    print(str(company_name) + " positive sentiment score " + str(positive_count))
+    print(str(company_name) + " negative sentiment score " + str(negative_count))
+    print(str(company_name) + " total word count " + str(count))
+   #print(str(company_name) + " average sentiment score per total number of words " + str(score_per_total_words))
+    print("Net sentiment score for " + str(company_name) + " is " + str(positive_count + negative_count))
+
+for company in companies:
+    company_name = company
+    get_sentiment_score(positive_count, negative_count)
+    print("----------------------------------------")
+
+
+# define x axis as company names and y axis as positive and negative sentiment scores
+
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+fig, ax = plt.subplots()
+
+ax.bar(listx, listpos, label='Positive', color='green')
+ax.bar(listx, listneg, color='red', label='Negative')
+ax.scatter(listx, listscore, color='black', label='Net Sentiment Score')
+
+ax.set_ylabel('Sentiment Score')
+ax.set_title('DOW 30 Companies Sentiment Score')
+ax.set_xticklabels(labels=listx,rotation=90)
+
+
+plt.show()
+
+
+
 
