6d4e55b19bf455b25c76e70bddca0a49936738fc
hennessyliam <liam.hennessy@outlook.com>
2022-11-28 13:01:00 -0500

    init

--- 6778 Docs/Sentiment_Analyzer-main/Download_Articles.py
+++ 6778 Docs/Sentiment_Analyzer-main/Download_Articles.py
@@ -7,12 +7,12 @@ import time
 options = webdriver.ChromeOptions()
 
 # Github credentials
-username = "username"
-password = "password"
+username = "li621332"
+password = "Joseph1102!"
 
 # 'American Express', 'Honeywell', 'Coca-Cola', 'Goldman Sachs', 'Cisco', 'Chevron', 'Caterpillar', 'Boeing', 'Apple', 'Amgen', '3M',  'Dow', 'Home Depot', 'IBM', 'Intel', , 'JPMorgan Chase', "McDonald's", 'Merck', 'Microsoft', 'Nike', 'Procter & Gamble', 'Salesforce', 'Travelers', 'UnitedHealth', 'Verizon', 'Visa', 'Walgreens Boots Alliance', 'Walmart', 'Walt Disney'
 
-companies = ['Amgen']
+companies = ['3M Co', 'American Express Co', 'Amgen Inc', 'Apple Inc', 'Boeing Co', 'Caterpillar Inc', 'Chevron Corp', 'Cisco Systems Inc', 'Coca-Cola Co', 'Dow Inc', 'Goldman Sachs Group Inc', 'Home Depot Inc', 'Honeywell International Inc', 'Intel Corp', 'International Business Machines Corp', 'Johnson & Johnson', 'JPMorgan Chase & Co', "McDonald's Corp", 'Merck & Co Inc', 'Microsoft Corp', 'Nike Inc', 'Procter & Gamble Co', 'Salesforce Inc', 'Travelers Companies Inc', 'UnitedHealth Group Inc', 'Verizon Communications Inc', 'Visa Inc', 'Walgreens Boots Alliance Inc', 'Walmart Inc', 'Walt Disney Co']
 
 
 def download_articles(company_name):
@@ -59,35 +59,68 @@ def download_articles(company_name):
     driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/date-selector/lng-search-options-menu/div[2]/div/custom-date-range-selector/input").click()
 
 
-    # change to 'news'
+    # change to 'news', then 'newspaper
     driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div/button").click()
     driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/filter-selector/lng-search-options-menu/div[2]/div/div/filter-selector-node[1]/label/span").click()
+    driver.find_element(By.XPATH, "//input[@data-track='Newspapers']").click()
 
     # click search button
     driver.find_element(By.XPATH, "//*[@id=\"3bJgkgk\"]/ln-gns-searchbox/lng-searchbox/div[1]/lng-search-button").click()
-    time.sleep(0.5)
-    # Select Articles 1-10
-
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[2]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[4]/label/input").click()
-    time.sleep(0.25)
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[6]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[8]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[10]/label/input").click()
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[12]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[14]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[16]/label/input").click()
-    time.sleep(0.25)
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[18]/label/input").click()
-    driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[20]/label/input").click()
-
+    time.sleep(1)
+    
+    # language selection:
+    # language button 
+    driver.find_element(By.XPATH, "//*[@id=\"podfiltersbuttonlanguage\"]").click()
     time.sleep(2)
+    # choose english language
+    driver.find_element('data-id', "pf63").click()
+    time.sleep(3)
+
+    # change geo location
+    driver.find_element(By.XPATH, "//*[@id=\"podfiltersbuttonen-geography-news\"]").click()
+    time.sleep(1)
+    driver.find_element(By.TAG_NAME, "North America").click()
+    time.sleep(1)
+
+    # change subject
+    driver.find_element(By.XPATH, "//*[@id=\"podfiltersbuttonen-subject\"]").click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//*[@id=\"refine\"]/ul[4]/li[1]/label").click()
+    time.sleep(1)
+
+    # box_list from values 2 - 20 interval 2
+    box_list = [2,4,6,8,10,12,14,16,18,20]
+    
+    # Select Articles 1-10
+    for i in box_list:
+
+        try:
+
+            driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li["+ str(i) +"]/label/input").is_displayed()
+            driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li["+ str(i) +"]/label/input").click()
+            time.sleep(1)
+
+        except:
+            break
+    
+    
+
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[2]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[4]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[6]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[8]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[10]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[12]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[14]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[16]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[18]/label/input").click()
+    # driver.find_element(By.XPATH, "//*[@id='content']/div[2]/form/div[2]/ol/li[20]/label/input").click()
+
 
     # click download button
     driver.find_element(By.XPATH, "//*[@id='results-list-toolbar-gvs']/ul[1]/li[4]/ul/li[3]/button/span[1]").click()
 
-    time.sleep(2)
+    time.sleep(1)
 
     # choose format 
     # .docx
